7166b4e670c9f2c04a827c836fbb91dc0352c6c4
Emilia Dobrin <dobrin@adobe.com>
2022-10-04 08:54:38 -0700

    Update order in toc

--- gatsby-config.js
+++ gatsby-config.js
@@ -464,16 +464,16 @@ module.exports = {
                 path: "/documentation/identity-for-edge-network",
                 pages: [
                     {
-                        title: "Release notes",
-                        path: "documentation/identity-for-edge-network/release-notes"
-                    },
-                    {
                         title: "API reference",
                         path: "documentation/identity-for-edge-network/api-reference"
                     },
                     {
                         title: "Frequently asked questions",
                         path: "documentation/identity-for-edge-network/faq"
+                    },
+                    {
+                        title: "Release notes",
+                        path: "documentation/identity-for-edge-network/release-notes"
                     }
                 ]
             },
