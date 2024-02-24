e152791b4a4bda26697586a267a26100cfa879fe
Christopher Hoffmann <cdhoffmann@users.noreply.github.com>
2024-02-23 17:14:15 -0700

    Update gatsby-config.js with privacy-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -789,6 +789,10 @@ module.exports = {
                     {
                         title: "Adobe Experience Cloud & Apple's IDFA, Privacy announcement",
                         path: "/resources/privacy-announcement"
+                    },
+                    {
+                        title: "iOS 17 & Privacy Manifests",
+                        path: "/resources/privacy-manifest.md"
                     }
 
                 ]
